558f79357614852fc640d3d7da22808720f17978
Shi <langtusayonly@gmail.com>
2023-05-24 12:25:06 +0700

    Banner #3

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import Container from '../Container'
-import { Logo, NavLink, Button } from '../navbar'
+import { Logo, NavLink, Button, Banner } from '../navbar'
 
 const Navbar = () => {
     return (
-        <div className="fixed w-full h-[900px] bg-pink-100">
+        <div className="fixed w-full h-[1000px] bg-pink-100">
             <div className="flex flex-row justify-center pt-8">
                 <div className="w-[90%] h-[100px] bg-white z-10 shadow-lg rounded-full">
                         <Container>
@@ -23,6 +23,7 @@ const Navbar = () => {
                         </Container>
                 </div>
             </div>
+            <Banner />
         </div>
 
     )
