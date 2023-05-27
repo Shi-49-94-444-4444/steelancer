2263d5ca26374051b33b6fc25709d31e12d74e98
Shi <langtusayonly@gmail.com>
2023-05-27 12:54:49 +0700

    Update router, fix reload page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import "./globals.css"
-import { Inter } from "next/font/google"
-import { Navbar, ClientOnly, Banner, Quick, Category, Recent, Background, AboutUs, Footer } from "./components"
-
-const inter = Inter({ subsets: ['latin'] })
+import { Navbar, ClientOnly, Background } from "./components"
 
 export const metadata = {
   title: 'Steelancer',
@@ -15,15 +12,11 @@ export default function Layout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className} style={{ overflowY: 'auto' }}>
-        <ClientOnly>
-          <Background>
-            <Navbar />
-            {children}
-          </Background>
-        </ClientOnly>
-      </body>
-    </html>
+    <ClientOnly>
+      <Background>
+        <Navbar />
+        {children}
+      </Background>
+    </ClientOnly>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/Login",
-        destination: "/src/Login",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
