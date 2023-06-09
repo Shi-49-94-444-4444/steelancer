9b8897ca7266de5c5427affb5e40e53a87bb288f
Shi <langtusayonly@gmail.com>
2023-06-09 19:20:10 +0700

    Revert "test ###"
    This reverts commit 74366ab0c7f80b42e3a0bbe268868fd91ecbf27b.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,22 @@
 import "./globals.css"
 import { Navbar, ClientOnly, Background } from "./components"
 import ToasterProvider from "./providers/ToasterProvider"
-import getCurrentUser from "./action/getCurrentUser"
 
 export const metadata = {
   title: 'Steelancer',
   description: 'Steelancer clone',
 }
 
-export default async  function Layout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const currentUser = await getCurrentUser();
-
   return (
     <ClientOnly>
       <Background>
         <ToasterProvider />
-        <Navbar currentUser={currentUser}/>
+        <Navbar />
         {children}
       </Background>
     </ClientOnly>
